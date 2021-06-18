6527a3c2e9b70c1d95c495ff16fa1e4731e04432
Ridwan Kolawole <olaniyi.jibola152@gmail.com>
2021-06-18 13:14:27 +0100

    CD testing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 var port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-    res.send("Implementation of travis CI and CD")
+    res.send("Implementation of CI and CD with travis CI")
 }) 
 
 
