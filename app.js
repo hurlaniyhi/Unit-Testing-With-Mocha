843c39f6263bf1bfa5cb091818d5d79204e1006b
Ridwan Kolawole <olaniyi.jibola152@gmail.com>
2021-06-18 15:04:46 +0100

    CI/CD integration with travis CI

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const app = express()
 var port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-    res.send("travis CI")
+    res.send("Implementing CI/CD using travis CI")
 }) 
-
 
 app.listen(port, ()=>{
     console.log(`server listening to port ${port}`)
 })
-
-//ae7edd5a-7e12-4b83-9f38-5b931e89676a
